b2ce3e73b98e550f684c52b363e4ba60daae7e6c
GCH230298 <sonntgch230298@fpt.edu.vn>
2026-07-03 03:13:18 +0700

    OMFG

--- APP/firebase/firebaseClient.py
+++ APP/firebase/firebaseClient.py
@@ -1,24 +1,14 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 import os
+import json
 
-cred = credentials.Certificate({
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_URL"),
-})
+service_account = json.loads(os.getenv("FIREBASE_SERVICE_ACCOUNT"))
+
+cred = credentials.Certificate(service_account)
 
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 print("firebase connected")
-
